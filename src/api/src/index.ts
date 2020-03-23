f9bf5d1cafc80cddf2c59d8106760310c77049e5
Brian Demant <brian.demant@gmail.com>
2020-03-23 16:34:55 +0100

    reset extra version

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -4,7 +4,6 @@ import * as express from 'express'
 import { applyMiddleware } from 'graphql-middleware'
 import { mocks } from './clients'
 import { DeprecatedDirective } from './directives'
-import { default as typeDefs2 } from './schema'
 // import { default as resolvers } from './resolvers'
 import { default as typeDefs } from './schemaV2'
 import { contextFn } from './schemaV2/context'
@@ -13,19 +12,6 @@ const options = { port: 2300 }
 
 const schema = makeExecutableSchema({
 	typeDefs,
-	resolvers: {},
-	// ignore missing resolvers
-	allowUndefinedInResolve: true,
-	resolverValidationOptions: {
-		requireResolversForArgs: false,
-		requireResolversForNonScalar: false,
-		requireResolversForAllFields: false,
-		requireResolversForResolveType: false,
-		allowResolversNotInSchema: false,
-	},
-})
-const schema2 = makeExecutableSchema({
-	typeDefs: typeDefs2,
 	resolvers: {},
 	// ignore missing resolvers
 	allowUndefinedInResolve: true,
@@ -64,35 +50,14 @@ const server = new ApolloServer({
 	// plugins: [TracingPlugin],
 })
 
-const server2 = new ApolloServer({
-	schema: applyMiddleware(schema2),
-	typeDefs: typeDefs2,
-	// prefers resolvers but uses mock if not implemented
-	mockEntireSchema: false,
-	mocks: {
-		SimpleID: () => 'SimpleID',
-		NonEmptyString: () => 'NonEmptyString',
-		// ...mocks,
-	},
-	resolvers: {},
-	schemaDirectives: {
-		deprecated: DeprecatedDirective,
-	},
-	tracing: true,
-	context: contextFn,
-	// plugins: [TracingPlugin],
-})
-
-const apolloMiddleware2 =
 
 app.use((req, res, next) => {
 	console.log()
-	if (`${req.headers['x-client-version']}`.match(/1/)) {
-		console.log('VERSION 1')
-		server.getMiddleware({ path: '/' })(req, res, next)
-	} else {
+	if (`${req.headers['x-client-version']}`.match(/2/)) {
 		console.log('VERSION 2')
-		server2.getMiddleware({ path: '/' })(req, res, next)
+	} else {
+		server.getMiddleware({ path: '/' })(req, res, next)
+		console.log('VERSION 1')
 	}
 })
 
